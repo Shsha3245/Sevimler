3589fa4a3fb9db656b60d83cc42d2d4aadd971a9
Shsha3245 <167920245+Shsha3245@users.noreply.github.com>
2026-04-17 00:28:33 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -151,4 +151,4 @@ def startup_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
