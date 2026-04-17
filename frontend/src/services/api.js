692f440d527e06de486fbc8ad4bce860bad7d215
Shsha3245 <167920245+Shsha3245@users.noreply.github.com>
2026-04-17 17:53:19 +0300

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://sevimler.onrender.com',
 });
 
 // Request interceptor to add token
