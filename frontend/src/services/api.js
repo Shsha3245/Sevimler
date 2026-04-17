b5ff7f328975d572450a5571c2be860e612a10a1
Shsha3245 <167920245+Shsha3245@users.noreply.github.com>
2026-04-17 17:54:34 +0300

    Update API base URL to new endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://sevimler.onrender.com',
+  baseURL: 'https://api.sevimlerkuruyemis.com',
 });
 
 // Request interceptor to add token
