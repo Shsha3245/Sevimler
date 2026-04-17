31569c01bc9a028c4b29d3aace1900b92647e321
Shsha3245 <167920245+Shsha3245@users.noreply.github.com>
2026-04-17 17:14:19 +0300

    Add type hint for password parameter in get_password_hash

--- backend/auth.py
+++ backend/auth.py
@@ -19,7 +19,8 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_password_hash(password):
+def get_password_hash(password:str):
+    password = password[:72]
     return pwd_context.hash(password)
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
