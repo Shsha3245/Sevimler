314af055ed9b20bdbbf514871a4c1ff3704958e0
Shsha3245 <167920245+Shsha3245@users.noreply.github.com>
2026-05-04 19:56:35 +0300

    Update paytr.py

--- backend/payment/paytr.py
+++ backend/payment/paytr.py
@@ -7,7 +7,7 @@ import logging
 MERCHANT_ID = os.getenv("PAYTR_MERCHANT_ID")
 MERCHANT_KEY = os.getenv("PAYTR_MERCHANT_KEY")
 MERCHANT_SALT = os.getenv("PAYTR_MERCHANT_SALT")
-TEST_MODE = os.getenv("PAYTR_TEST_MODE", "1")  # canlıda "0" olmalı
+TEST_MODE = os.getenv("PAYTR_TEST_MODE", "0")  # canlıda "0" olmalı
 
 APP_ENV = os.getenv("ENV", "development")
 
