9d9876791473537db89effe9066ffc4502dcc74b
Shsha3245 <167920245+Shsha3245@users.noreply.github.com>
2026-05-04 19:43:54 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -66,9 +66,7 @@ async def log_requests(request: Request, call_next):
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://sevimlerkuruyemis.com",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "https://sevimlerkuruyemis.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
